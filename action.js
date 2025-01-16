804b4a1dc328f2c17b79024e9997cd4b34c12191
MaroAF <madaroby99@gmail.com>
2025-01-16 00:02:55 -0600

    Imagenes

--- action.js
+++ action.js
@@ -7,7 +7,14 @@ $( document ).ready(function() {
 
       $(".razon1").on( "click", function() {
         $(".razon1").slideToggle(1000, function(){
-            // $(".razon1").slideToggle(1000)
+             $(".razon2").slideToggle(1000)
         });
       } );
+
+      $(".razon2").on( "click", function() {
+        $(".razon2").slideToggle(1000, function(){
+             $(".razon3").slideToggle(1000)
+        });
+      } );
+
 });
